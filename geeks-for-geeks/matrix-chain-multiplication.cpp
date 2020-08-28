71246abc83b0c665b46e77dbfd5e69672a9f26c2
sahutkarsh@gmail.com <sahutkarsh@gmail.com>
2020-08-29 01:59:59 +0530

    Matrix Chain Multiplication

---
+++ geeks-for-geeks/matrix-chain-multiplication.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int matrixChain(int *arr, int n) {
+    vector<vector<int>> dp(n, vector<int> (n, 0));
+    for (int i=1; i<n; i++) {
+        for (int j=0; j<n-i; j++) {
+            int x = j;
+            int y = i+j;
+            int minComp = INT_MAX;
+            for (int i=x; i<y; i++) {
+                int current = dp[x][i] + dp[i+1][y];
+                current += arr[x] * arr[i+1] * arr[y+1];
+                minComp = min(minComp, current);
+            }
+            dp[x][y] = minComp;
+        }
+    }
+    return dp[0][n-1];
+}
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--) {
+	    int n;
+	    cin >> n;
+	    int arr[n];
+	    for (int i=0; i<n; i++) cin >> arr[i];
+	    int res = matrixChain(arr, n-1);
+	    cout << res << endl;
+	}
+	return 0;
+}
